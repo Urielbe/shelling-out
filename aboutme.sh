60736a1286595ea66e2282d72ce87fd65b676661
Urielbe <benatar87@gmail.com>
2018-12-30 23:59:17 +0200

    HomeWork

---
+++ aboutme.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#################################
+#by uriel
+#30.12.18
+#
+#details about myself
+#################################
+
+fname=Uriel
+lname=Benatar
+uname=urielbe
+dob=18/1/87
+mail=benatar87@gmail.com
+
+printf "%s \n" $fname
+printf "%s \n" $lname
+printf "%s \n" $uname
+printf "%s \n" $dob
+printf "%s \n" $mail
+
+echo "#############DONE############"
---
+++ var.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+###########################
+#by uriel
+#12.30.18
+#
+#print Variables
+##########################
+
+echo "Home folder:" $HOME
+echo "User:" $USER
+echo "Display:" $DISPLAY
+echo "Term:" $TERM
+echo "Path:" $PATH
+echo "Histsize:" $HITSTSIZE
+echo "Session Manager:" $SESSION_MANAGER
+echo "Shell:" $SHELL
+echo "Host name:" $HOSTNAME
+echo "Language:" $LANG
+echo "Mail:" $MAIL
+
+
